80545b097e2a740af7f48c4ddd3a736d4f83a3fb
Azima Motaghi <azima.m@gmail.com>
2017-01-11 09:21:09 -0600

    Create OnCall.vb

---
+++ OnCall.vb
@@ -0,0 +1,69 @@
+sub SubEmail1()
+    
+   Dim obj
+   Set obj=server.FindObject("Brazos.~Email Redirection.E-Mail Action 1")
+   		IF obj.Interface.InService = 0 then
+   		
+			obj.Interface.InService = 1
+			MsgBox "Cody Jimenez is On-Call" , vbOKOnly, "On-Call"
+		ELSE 
+		
+			obj.Interface.InService = 0
+		END If
+
+End Sub
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+sub SubEmail2()
+    
+   Dim obj
+   Set obj=server.FindObject("Brazos.~Email Redirection.E-Mail Action 2")
+   		IF obj.Interface.InService = 0 then
+   	
+			obj.Interface.InService = 1 
+			MsgBox "Josh Gaddis is On-Call" , vbOKOnly, "On-Call"
+		ELSE obj.Interface.InService = 0
+		END If
+	
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+sub SubEmail3()
+    
+   Dim obj
+   Set obj=server.FindObject("Brazos.~Email Redirection.E-Mail Action 3")
+   		IF obj.Interface.InService = 0 then
+   	
+			obj.Interface.InService = 1 
+			MsgBox "Jr Cordova is On-Call" , vbOKOnly, "On-Call"
+		ELSE obj.Interface.InService = 0
+		END If
+	
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+sub SubEmail4()
+    
+   Dim obj
+   Set obj=server.FindObject("Brazos.~Email Redirection.E-Mail Action 4")
+   		IF obj.Interface.InService = 0 then
+   	
+			obj.Interface.InService = 1 
+			MsgBox "Jason Merrick is On-Call" , vbOKOnly, "On-Call"
+		ELSE obj.Interface.InService = 0
+		END If
+	
+End Sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+sub SubEmail5()
+    
+   Dim obj
+   Set obj=server.FindObject("Brazos.~Email Redirection.E-Mail Action 5")
+   		IF obj.Interface.InService = 0 then
+   	
+			obj.Interface.InService = 1 
+			MsgBox "Greg Watson  is On-Call" , vbOKOnly, "On-Call"
+		ELSE obj.Interface.InService = 0
+		END If
+	
+End Sub
